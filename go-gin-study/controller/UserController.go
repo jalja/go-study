84c5915a3fa78e604e82e27e1da44c632f9b01ff
xulei <jalja365@163.com>
2023-04-27 15:51:17 +0800

    gin

--- go-gin-study/controller/UserController.go
+++ go-gin-study/controller/UserController.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-gin-study/model/param"
 	"go-gin-study/model/response"
 	"go-gin-study/tools"
 	"net/http"
@@ -14,11 +15,24 @@ func (user *UserController) Router(router *gin.Engine) {
 	//engine.Group 路由组 (http://127.0.0.1:8080/user/getUser)
 	userGroup := router.Group("/user")
 	userGroup.GET("/getUserById", user.getUserById)
+	userGroup.POST("/login", user.login)
 }
 
 // 更具条件查询 UserInfo
 func (user *UserController) getUserById(context *gin.Context) {
 	id := context.Query("id")
 	tools.Log.Infof("UserController-getUserById=>id=%s", id)
+	context.JSON(http.StatusOK, response.Success(""))
+}
+
+// 登录接口
+func (user *UserController) login(context *gin.Context) {
+	var loginFrom param.LoginFrom
+	err := context.ShouldBind(&loginFrom)
+	if err != nil {
+		tools.Log.Error("UserController-login=>解析请求参数异常")
+		return
+	}
+	tools.Log.Infof("UserController-login=>入参=%s", loginFrom)
 	context.JSON(http.StatusOK, response.Success(""))
 }
